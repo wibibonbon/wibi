d329854eeed32f4f099eb6accbd74bfd42d8e320
Wibke Baudach <baudach.sw@gmail.com>
2019-11-05 00:31:57 +0800

    modal horizontal position fix for firefox

--- src/components/PortModal.vue
+++ src/components/PortModal.vue
@@ -150,10 +150,10 @@ export default {
     z-index: 1000;
     top: 0;
     left: 0;
-    width: 100%;
-    height: 100%;
+    width: 100vw;
+    height: 100vh;
     background-color: rgba(0, 0, 0, 0.5);
-    display: grid;
+    display: table;
     transition: opacity 0.3s ease;
 }
 
